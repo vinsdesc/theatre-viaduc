11fc3fda37f33b9028e6561d34678c9e703f02dd
vinsdesc <delphinebellante@hotmail.com>
2017-10-11 13:05:57 +0200

    text

--- theatre/index.php
+++ theatre/index.php
@@ -2,24 +2,14 @@
 <html>
 		<?php include 'head.php';?>
 	<body>
-		<style type="text/css">
-			a, a:focus, a:active, a:visited {    color: #c73333;
-    text-decoration: none;
-    font-size: 40px;
-    z-index: 1;
-    text-decoration:none;
-    display: block;
-    }
-
-
 
 		</style>
 		<article>
 			<img  class="img-responsive" src="asset/sonette.png" alt="sonette"> 
-			<!-- <div class="btn-group-vertical"> -->
+		<div class="bouton"> 
 				<a href="theatre1.php"  role="button">Théâtre</a>
 				<a href="musee.php"  role="button">Autre musée</a>
-			<!-- </div> -->
+		</div> 
 		</article>
 
 	</body>
--- theatre/theatre1.php
+++ theatre/theatre1.php
@@ -6,34 +6,37 @@
 
 	<body>
 		<?php include 'accueil.php';?>
-			
+		<div class="btn-group">
+	  		<button type="button" class="btn btn-primary">micro</button>
+	 		<button type="button" class="btn btn-primary">recherche</button>
+	  		<button type="button" id="oeil" class="btn btn-primary">oeil</button>
+		</div>
+		
+
 		<video id="videoarea" poster="" src=""></video>
 
 		<ul id="playlist">
-		    <li movieurl="video/demo.mp4"> <button   class="objet" id="chandelier" class="play" > </button> </li>
-		    <li movieurl="video/demo.mp4"> <button   class="objet" id="lampe" class="play" > </button> </li>          
-		    <li movieurl="video/demo.mp4"> <button   class="objet" id="chaussure" class="play" > </button></li>   
-		    <li movieurl="video/demo.mp4"> <button   class="objet" id="cafetiere" class="play" > </button></li> 
-		    <li movieurl="video/demo.mp4"> <button   class="objet" id="chaise" class="play" > </button></li> 
-		    <li movieurl="video/demo.mp4"> <button   class="objet" id="ombrelle" class="play" > </button></li> 
-		     <li movieurl="video/demo.mp4"> <button  class="objet" id="phono" class="play" > </button></li>    
+		    <li movieurl="video/demo.mp4"> 
+		    	<button   class="objet" id="chandelier" > </button> 
+				<p style="display: none" id="chandelier1">lalalaalal</p>
+		    </li>
+		    <li movieurl="video/demo.mp4"> <button   class="objet" id="lampe" > </button> </li>          
+		    <li movieurl="video/demo.mp4"> <button   class="objet" id="chaussure" > </button></li>   
+		    <li movieurl="video/demo.mp4"> <button   class="objet" id="cafetiere" > </button></li> 
+		    <li movieurl="video/demo.mp4"> <button   class="objet" id="chaise" > </button></li> 
+		    <li movieurl="video/demo.mp4"> <button   class="objet" id="ombrelle" > </button></li> 
+		    <li movieurl="video/demo.mp4"> <button  class="objet" id="phono" > </button></li>    
 		</ul>
-		<button onclick="playPause()">Play/Pause</button> 
-	</body>
-			<script>
-				var myVideo = document.getElementById("videoarea"); 
+		<button type="button" id="play" class="btn btn-secondary" onclick="playPause()">play</button>
 
-			function playPause() { 
-			    if (myVideo.paused) 
-			        myVideo.play(); 
-			    else 
-			        myVideo.pause(); 
-			} 
-/*			//video cachée au démarage, theatre1.php
+	</body>
+		<script>
+
+		//video cachée au démarage, theatre1.php
 			$("video").hide();
 			$(".objet").click(function(){
 		    $("video").show();
-			});*/
+			});
 		/*la fenetre video disparait lorsqu'il y a un bouton sonore
 		$("#lit").click(function(){
 		    $("video").hide();
